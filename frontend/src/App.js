33f465624abadfee56bbdb26c3fe5f2addaedc1d
hao-zhang-05 <hao@mmtech.com.au>
2025-08-27 17:46:40 +1000

    different UI for logged in and logged out

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,14 +13,17 @@ import NewEventPage from './pages/NewEvent';
 import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
-import AuthenticationPage, {action as authAction} from './pages/Authentication';
-import {action as logoutAction} from './pages/logout';
+import AuthenticationPage, { action as authAction } from './pages/Authentication';
+import { action as logoutAction } from './pages/logout';
+import { tokenLoader } from './util/auth';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
     errorElement: <ErrorPage />,
+    id: 'root',
+    loader: tokenLoader,
     children: [
       { index: true, element: <HomePage /> },
       {
